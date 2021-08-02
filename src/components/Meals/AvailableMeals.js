5cda0d1ccb043bd3aad44ff88f4b897586094983
neoruien <neoruien123@gmail.com>
2021-08-02 15:12:00 +0800

    Add landing page

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,25 @@
+import React from "react";
+import DUMMY_MEALS from "./dummy-meals";
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
+
+function AvailableMeals() {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeals;
