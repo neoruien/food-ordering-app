07c822b2add2e800f81fe3ac76e17f482b9ec9a2
neoruien <neoruien123@gmail.com>
2021-08-02 15:43:30 +0800

    Add pop up component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = () => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+function Modal(props) {
+  return (
+    <div>
+      <React.Fragment>
+        {ReactDOM.createPortal(<Backdrop />, portalElement)}
+        {ReactDOM.createPortal(
+          <ModalOverlay>{props.children}</ModalOverlay>,
+          portalElement
+        )}
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default Modal;
